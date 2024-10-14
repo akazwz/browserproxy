6afeceb41392294400ff6fe3fc8a7dc21c9659ff
akazwz <50396286+akazwz@users.noreply.github.com>
2024-10-14 21:25:23 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
-import { Elysia } from "elysia";
+import { Hono } from "hono";
+import { createBunWebSocket } from "hono/bun";
+import type { ServerWebSocket } from "bun";
+import puppeteer from "puppeteer-core";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const { upgradeWebSocket, websocket } = createBunWebSocket<ServerWebSocket>();
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+const app = new Hono();
+
+const endpoints: string[] = [];
+
+app.get(
+	"/",
+	upgradeWebSocket(async(c) => {
+		const token = c.req.query("token");
+		if (token !== process.env.TOKEN) {
+			throw new Response("Unauthorized", { status: 401 });
+		}
+		if (endpoints.length === 0) {
+			console.log("launching puppeteer")
+			const brower = await puppeteer.launch({
+				channel: "chrome",
+				executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+				args: ["--no-sandbox", "--disable-setuid-sandbox"],
+			})
+			endpoints.push(brower.wsEndpoint())
+		}
+		const randomEndpoint = endpoints[Math.floor(Math.random() * endpoints.length)];
+        const socket = new WebSocket(randomEndpoint)
+        
+		return {
+			onOpen(evt, ws) {
+				socket.onmessage = (evt) => {
+                    ws.send(evt.data)
+                }
+			},
+			onMessage(evt, ws) {
+                socket.send(evt.data)
+			},
+		};
+	}),
 );
+
+export default {
+	fetch: app.fetch,
+	websocket,
+};
